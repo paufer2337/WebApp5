ffe80cc0183aec1528fba6b4c23b1e54809ff1a2
paufer2337 <paufer2337@gmail.com>
2024-08-21 23:15:15 +0200

    modified header and searchbar, added logo

--- reactapp/src/jsx/header.jsx
+++ reactapp/src/jsx/header.jsx
@@ -1,11 +1,10 @@
 import React, { useContext } from "react";
-
 import "../css/header.css";
-
 import ThemeContext from "./ThemeContext";
 import ColorModeSelector from "./colorModeSelector.jsx";
 import LoginButton from "./loginButton.jsx";
 import RegisterButton from "./registerButton.jsx";
+import logo from '../img/w5_logo.png';  
 
 function Header() {
   const { theme } = useContext(ThemeContext);
@@ -16,34 +15,36 @@ function Header() {
         className={`nav ${
           theme === "dark" ? "dark-theme-header" : "light-theme-header"
         }`}>
-        <ul
-          className={`ul ${
-            theme === "dark" ? "dark-theme-list" : "light-theme-list"
-          }`}>
-          <li className="nav-item">
-            <a
-              className="nav-link text-dark"
-              href="/">
-              Hem
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              className="nav-link text-dark"
-              href="/">
-              Sekretesspolicy
-            </a>
-          </li>
-          <li className="nav-item">
-            <RegisterButton></RegisterButton>
-          </li>
-          <li className="nav-item">
-            <LoginButton></LoginButton>
-          </li>
-          <li className="nav-item">
-            <ColorModeSelector></ColorModeSelector>
-          </li>
-        </ul>
+        <div className="nav-left">
+          <ul className={`ul ${theme === "dark" ? "dark-theme-list" : "light-theme-list"}`}>
+            <li className="nav-item">
+              <a className="nav-link text-dark" href="/">HEM</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-dark" href="/">SEKRETESSPOLICY</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="logo-container">
+          <a href="/">
+            <img src={logo} alt="Logo" className="logo" /> 
+          </a>
+        </div>
+
+        <div className="nav-right">
+          <ul className={`ul ${theme === "dark" ? "dark-theme-list" : "light-theme-list"}`}>
+            <li className="nav-item">
+              <RegisterButton />
+            </li>
+            <li className="nav-item">
+              <LoginButton />
+            </li>
+            <li className="nav-item">
+              <ColorModeSelector />
+            </li>
+          </ul>
+        </div>
       </nav>
     </header>
   );
